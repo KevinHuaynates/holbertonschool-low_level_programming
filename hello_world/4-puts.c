754fe42f46db9e310842739dafd82f1476e728c0
KevinHuaynates <6992@holbertonstudents.com>
2023-10-17 04:08:29 -0700

    Puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+/**
+* main - puts Programming is like building a multilingual puzzle
+* Description: prints Programming is like building
+* Return:0
+*/
+
 int main(void)
-	
 {
-	    puts("\"Programming is like building a multilingual puzzle");
-	        return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
 
