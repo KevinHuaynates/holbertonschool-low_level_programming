28f7e875e05cf88986860d9a38a7304817e52f37
KevinHuaynates <6992@holbertonstudents.com>
2023-10-20 04:42:13 -0700

    ultimo digito

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,7 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* betty style doc for function main goes there */
+/**
+* main - Imprime el ultimo digito
+* Description: Muestra si el ultimo digito es <,> o = a 5
+* Return:0
+*/
+
 int main(void)
 {
 		int n;
@@ -10,19 +15,18 @@ int main(void)
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
 				ultimo_digito = abs(n) % 10;
-				printf("Last digit of %d is %d", n, ultimo_digito); 
-					if (ultimo_digito >5 )
+				printf("Last digit of %d is %d", n, ultimo_digito);
+					if (ultimo_digito > 5)
 					{
-						printf("and is greater than 5\n");  
+					printf(" and is greater than 5\n");
 					}
-					else if(ultimo_digito == 0 )
+					else if (ultimo_digito == 0)
 					{
-						 printf(" and is 0\n");
+					printf(" and is 0\n");
 					}
-					 else
-					 {
-						  printf(" and is less than 6 and not 0\n");	 
-					 }
-
-					return (0);
+					else
+					{
+					printf(" and is less than 6 and not 0\n");
+					}
+				return (0);
 }
