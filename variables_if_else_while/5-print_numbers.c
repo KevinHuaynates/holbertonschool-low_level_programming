34b88ec56e97274634edb35b0159530a06d7aaae
KevinHuaynates <6992@holbertonstudents.com>
2023-10-20 05:49:17 -0700

    inprimir numeros

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*  main - puts Programming is like building a multilingual puzzle
+* Description: prints Programming is like building
+* Return:0
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 11)
+	{
+	printf("%d", num);
+	num++;
+	}
+	return (0);
+}
