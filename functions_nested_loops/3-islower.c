30d89b97295541d98611684f4a8d7d11ffab68d3
KevinHuaynates <6992@holbertonstudents.com>
2023-10-22 17:25:44 -0700

    is lower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main -
+* Description:
+* Return:0
+*/
+
+int _islower(int c)
+{
+	    if (c >= 'a' && c <= 'z')
+		        {
+				        return (1);
+					    }
+	        else
+			    {
+				            return (0);
+					        }
+}
+
