2e7d3fd7c134c99a4699b6ecc97ae21de76c3ab7
KevinHuaynates <6992@holbertonstudents.com>
2023-10-22 19:57:34 -0700

    triangulo

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -2,21 +2,17 @@
 
 void print_triangle(int size)
 {
-	    if (size <= 0)
-		        {
-				        _putchar('\n');
-					        return;
-						    }
-
-	        for (int i = 1; i <= size; i++)
-			    {
-				            for (int j = 0; j < size - i; j++)
-						                _putchar(' ');
-
-					            for (int k = 0; k < i; k++)
-							                _putchar('#');
-
-						            _putchar('\n');
-							        }
+if (size <= 0)
+{
+_putchar('\n');
+return;
 }
-
+for (int i = 1; i <= size; i++)
+{
+for (int j = 0; j < size - i; j++)
+_putchar(' ');
+for (int k = 0; k < i; k++)
+_putchar('#');
+_putchar('\n');
+}
+}
