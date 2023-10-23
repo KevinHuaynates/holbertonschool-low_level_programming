f2b2d1c34396e550258678166dbdff406f948b2f
KevinHuaynates <6992@holbertonstudents.com>
2023-10-22 18:41:48 -0700

    isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,19 @@
 #include "main.h"
-
+/**
+* _isupper - revisa caracteres correctos
+* @c: Revisa si los caracteres son correctos
+*
+* Description: Comprueba si los numeros son mayusculas
+* Return: 0
+*/
 int _isupper(int c)
 {
-	    if (c >= 'A' && c <= 'Z')
-		        {
-				        return (1);
-					    }
-	        else
-			    {
-				            return (0);
-					        }
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
 }
-
