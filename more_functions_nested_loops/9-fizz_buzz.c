357262d88b7744c6a4fd2f13e5143289cbba9df6
KevinHuaynates <6992@holbertonstudents.com>
2023-10-22 20:11:09 -0700

    Fizz Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-	    int i;
-	        for (i = 1; i <= 100; i++)
+int i;
+for (i = 1; i <= 100; i++)
 			    {
 				            if (i % 3 == 0 && i % 5 == 0)
 						                printf("FizzBuzz ");
