e658cf6a9ca4f522a4eb3a2cf9108d4f647ec94f
KevinHuaynates <6992@holbertonstudents.com>
2023-10-22 17:06:06 -0700

    alfabeto

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,14 +3,15 @@
 * print_alphabet- imprime el alfabeto
 * main- es el archivo principa
 * Description:Inprimir con putchar
-* Return:0
+* Return:
 */
 	void print_alphabet(void)
 {
 	char c;
-	for (c = 'a'; c <= 'z'; ++c)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
 	}
 	_putchar('\n');
+
 }
