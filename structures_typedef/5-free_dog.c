d238dcab6c9a521430b4b372e080ae8a5ea4e7f4
KevinHuaynates <6992@holbertonstudents.com>
2023-11-05 18:52:04 -0800

    ejercicio 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ *  * free_dog - frees a dog
+ *   * @d: pointer to the dog to free
+ *    */
+void free_dog(dog_t *d)
+{
+	    if (d != NULL)
+		        {
+				        /* free the name string */
+				        free(d->name);
+					        /* free the owner string */
+					        free(d->owner);
+						        /* free the dog structure */
+						        free(d);
+							    }
+}
+
