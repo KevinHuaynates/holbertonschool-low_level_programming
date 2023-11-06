1060b40b7602a633d306319a922f64438c88d4f6
KevinHuaynates <6992@holbertonstudents.com>
2023-11-05 18:19:09 -0800

    ejercicio 2

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,12 @@
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+	};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #include "main.h"
-#include "dog.h"
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
