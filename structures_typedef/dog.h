9738dddf94a739d59c25e04561268a6d7d7f5fec
KevinHuaynates <6992@holbertonstudents.com>
2023-11-05 18:25:32 -0800

    ejercicio 2

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+
 struct dog
 {
     char *name;
     float age;
     char *owner;
 };
+
+void init_dog(struct dog *d, char *nombre, float edad, char *propietario);
+
+#endif
+
