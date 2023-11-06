5867668ec3ac81b2beb9e4f62d02e407906bc64b
KevinHuaynates <6992@holbertonstudents.com>
2023-11-05 18:35:22 -0800

    ejercicio 3

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "dog.h"
 
+
 void print_dog(struct dog *d)
 {
 	    if (d != NULL) {
-  printf("Nombre: %s\n", d->nombre != NULL ? d->nombre : "(nil)");
-  printf("Edad: %.6f\n", d->edad);
-  printf("Propietario: %s\n", d->propietario != NULL ? d->propietario : "(nil)");
+		            printf("Nombre: %s\n", d->name != NULL ? d->name : "(nil)");
+			            printf("Edad: %.6f\n", d->age);
+				            printf("Propietario: %s\n", d->owner != NULL ? d->owner : "(nil)");
 					        }
-}i
+}
+
