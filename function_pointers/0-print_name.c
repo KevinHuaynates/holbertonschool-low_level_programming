4ac178f6f3f5a57ce562acd9c6bed363ffa5eb0c
KevinHuaynates <6992@holbertonstudents.com>
2023-11-05 19:10:26 -0800

    ejercicio 1

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ *  * print_name - prints a name using a function pointer
+ *   * @name: name of the person
+ *    * @f: pointer to a function that prints a name
+ *     * Return: Nothing.
+ *      */
+void print_name(char *name, void (*f)(char *))
+{
+	    if (name != NULL && f != NULL)
+		        {
+				        /* call the function pointer with the name as an argument */
+				        f(name);
+					    }
+}
+
