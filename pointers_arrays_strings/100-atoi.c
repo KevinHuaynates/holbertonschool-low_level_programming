19dee44228f176a74ec271ff170cefae9dae9884
KevinHuaynates <6992@holbertonstudents.com>
2023-12-27 21:35:18 -0800

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,34 +8,27 @@
  */
 int _atoi(char *s)
 {
+int i = 0;
 int sign = 1;
 int result = 0;
-int i = 0;
 
-	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
-	i++;
-
-	while (s[i] == '-' || s[i] == '+')
+	while (s[i] != '\0')
 	{
-	if (s[i] == '-')
+		if (s[i] == '-')
+		{
 		sign *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				result = result * 10 + (s[i] - '0');
+				i++;
+			}
+			break;
+		}
 		i++;
 	}
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-	if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > 7))
-	{
-	if (sign == 1)
-	return (INT_MAX);
-	else
-	return (INT_MIN);
-	}
-
-	result = result * 10 + (s[i] - '0');
-	i++;
-	}
-
-return (result * sign);
+	return (result * sign);
 }
 
