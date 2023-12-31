7e588f77cad822e2c1b02a84b8c274ef8cbd78b8
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 02:20:00 -0800

    3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,41 +3,33 @@
 #include "3-calc.h"
 
 /**
- *  * main - performs simple operations
- *   * @argc: number of arguments passed to the program
- *    * @argv: array of arguments passed to the program
- *     * Return: 0 if successful, otherwise returns other status values depending on error
- *      */
+** main - performs simple operations
+** @argc: number of arguments passed to the program
+** @argv: array of arguments passed to the program
+** Return: 0 if successful,values depending on error
+**/
 int main(int argc, char *argv[])
 {
-	    int num1, num2;
-	        int (*operation)(int, int);
-
-		    if (argc != 4)
-			        {
-					        printf("Error\n");
-						        exit(98);
-							    }
-
-		        num1 = atoi(argv[1]);
-			    num2 = atoi(argv[3]);
-
-			        operation = get_op_func(argv[2]);
-
-				    if (operation == NULL)
-					        {
-							        printf("Error\n");
-								        exit(99);
-									    }
-
-				        if ((operation == op_div || operation == op_mod) && num2 == 0)
-						    {
-							            printf("Error\n");
-								            exit(100);
-									        }
-
-					    printf("%d\n", operation(num1, num2));
-
-					        return (0);
+int num1, num2;
+int (*operation)(int, int);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
 }
-
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+operation = get_op_func(argv[2]);
+if (operation == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+if ((operation == op_div || operation == op_mod) && num2 == 0)
+{
+printf("Error\n");
+exit(100);
+}
+printf("%d\n", operation(num1, num2));
+return (0);
+}
