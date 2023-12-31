ffdb4e8715091b5157c1d6696350e7343ab848a5
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 01:49:38 -0800

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,32 +13,27 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
-
-    /* Allocate memory for the new dog */
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return NULL;
-
-    /* Allocate memory for the name and owner and copy the data */
-    new_dog->name = strdup(name);
-    if (new_dog->name == NULL)
-    {
-        free(new_dog);
-        return NULL;
-    }
-
-    new_dog->owner = strdup(owner);
-    if (new_dog->owner == NULL)
-    {
-        free(new_dog->name);
-        free(new_dog);
-        return NULL;
-    }
-
-    /* Assign the age */
-    new_dog->age = age;
-
-    return new_dog;
+dog_t *new_dog;
+/* Allocate memory for the new dog */
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+return (NULL);
+/* Allocate memory for the name and copy the data */
+new_dog->name = strdup(name);
+if (new_dog->name == NULL)
+{
+free(new_dog);
+return (NULL);
 }
-
+/* Allocate memory for the owner and copy the data */
+new_dog->owner = strdup(owner);
+if (new_dog->owner == NULL)
+{
+free(new_dog->name);
+free(new_dog);
+return (NULL);
+}
+/* Assign the age */
+new_dog->age = age;
+return (new_dog);
+}
