7c480c1731f4bb57dd6d36d40e98b9b251eb5513
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 01:57:31 -0800

    5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -7,14 +7,13 @@
  *    */
 void free_dog(dog_t *d)
 {
-	    if (d != NULL)
-		        {
-				        /* free the name string */
-				        free(d->name);
-					        /* free the owner string */
-					        free(d->owner);
-						        /* free the dog structure */
-						        free(d);
-							    }
+if (d != NULL)
+{
+/* free the name string */
+free(d->name);
+/* free the owner string */
+free(d->owner);
+/* free the dog structure */
+free(d);
 }
-
+}
