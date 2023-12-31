fdfbe20d828098eda42c09cd2644e14ccf2670c9
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 01:09:08 -0800

    1-main.c

--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
-int main(void)
+ * print_alphabet - Prints the alphabet in lowercase followed by a new line.
+ */
+void print_alphabet(void)
 {
-	    print_alphabet();
-	        return (0);
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        _putchar(letter);
+        letter++;
+    }
+
+    _putchar('\n');
 }
