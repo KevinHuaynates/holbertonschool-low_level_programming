a6753c917e7a2de45ad268bab428720f7a9703b7
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 01:42:00 -0800

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>  // Include the header for strdup
 #include "dog.h"
 
 /**
@@ -13,32 +12,27 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
+dog_t *new_dog;
 
-    /* Allocate memory for the new dog */
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
-
-    /* Allocate memory for the name and owner and copy the data */
-    new_dog->name = strdup(name);
-    if (new_dog->name == NULL)
-    {
-        free(new_dog);
-        return (NULL);
-    }
-
-    new_dog->owner = strdup(owner);
-    if (new_dog->owner == NULL)
-    {
-        free(new_dog->name);
-        free(new_dog);
-        return (NULL);
-    }
-
-    /* Assign the age */
-    new_dog->age = age;
-
-    return (new_dog);
+/* Allocate memory for the new dog */
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+return (NULL);
+/* Allocate memory for the name and owner and copy the data */
+new_dog->name = strdup(name);
+if (new_dog->name == NULL)
+{
+free(new_dog);
+return (NULL);
 }
-
+new_dog->owner = strdup(owner);
+if (new_dog->owner == NULL)
+{
+free(new_dog->name);
+free(new_dog);
+return (NULL);
+}
+/* Assign the age */
+new_dog->age = age;
+return (new_dog);
+}
