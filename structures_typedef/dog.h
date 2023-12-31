a81ed020d25830676c094fac6fe9cda71f254fec
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 01:32:11 -0800

    3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,12 +9,17 @@
  */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
 
+/* Typedef for struct dog as dog_t */
+typedef struct dog dog_t;
+
+/* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 #endif /* DOG_H */
+
