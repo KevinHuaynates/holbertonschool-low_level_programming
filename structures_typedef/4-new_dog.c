0d357ace032bc637f27d4d0349687d44134f96c4
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 01:36:30 -0800

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,48 +1,38 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
- *  * new_dog - creates a new dog
- *   * @name: name of the dog
- *    * @age: age of the dog
- *     * @owner: owner of the dog
- *      * Return: a pointer to the new dog
- *       */
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: A pointer to the newly created dog_t structure.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	    dog_t *dog;
-	        char *name_copy, *owner_copy;
+dog_t *new_dog;
 
-		    /* allocate memory for the dog structure */
-		    dog = malloc(sizeof(dog_t));
-		        if (dog == NULL)
-				        return (NULL);
-
-			    /* allocate memory for the name string and copy it */
-			    name_copy = malloc(strlen(name) + 1);
-			        if (name_copy == NULL)
-					    {
-						            free(dog);
-							            return (NULL);
-								        }
-				    strcpy(name_copy, name);
-
-				        /* allocate memory for the owner string and copy it */
-				        owner_copy = malloc(strlen(owner) + 1);
-					    if (owner_copy == NULL)
-						        {
-								        free(name_copy);
-									        free(dog);
-										        return (NULL);
-											    }
-					        strcpy(owner_copy, owner);
-
-						    /* assign the fields of the dog structure */
-						    dog->name = name_copy;
-						        dog->age = age;
-							    dog->owner = owner_copy;
-
-							        /* return the pointer to the new dog */
-							        return (dog);
+/* Allocate memory for the new dog */
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+return (NULL);
+/* Allocate memory for the name and owner and copy the data */
+new_dog->name = strdup(name);
+if (new_dog->name == NULL)
+{
+free(new_dog);
+return (NULL);
+}
+new_dog->owner = strdup(owner);
+if (new_dog->owner == NULL)
+{
+free(new_dog->name);
+free(new_dog);
+return (NULL);
+}
+/* Assign the age */
+new_dog->age = age;
+return (new_dog);
 }
